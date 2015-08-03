a5b0133dc77092dfb618b1cc4602823de377ab8c
Melisa Agata <agatamelisa@gmail.com>
2015-08-03 01:38:44 -0700

    finished initial css rework

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require owl.carousel
 //= require turbolinks
 //= require_tree .
